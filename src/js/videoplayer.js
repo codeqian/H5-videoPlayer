446ed2b9cbbcd28692cc8783b6a76c9cbc505535
qianzd <qian.wolf@gmail.com>
2015-12-31 18:02:44 +0800

    视频监听

--- src/js/videoplayer.js
+++ src/js/videoplayer.js
@@ -1,9 +1,24 @@
 function init() {
-    document._video = document.getElementById("mainPlayer");
+	window._duration=0;
+	window._curtime=0;
+    document._video = document.getElementById("mainVideo");
     document._playBtn=document.getElementById("playBtn");
     document._mutedBtn=document.getElementById("mutedBtn");
     document._fullscreenBtn=document.getElementById("fullscreenBtn");
+    document._currenttime=document.getElementById("currenttime");
+    document._alltime=document.getElementById("alltime");
+    document._progressBar=document.getElementById("progressBar");
+    document._volumeBar=document.getElementById("volumeBar");
     document._video.controls = false;
+    //获得视频长度
+    var i=setInterval(function() {
+		if(document._video.readyState > 0) {
+			document._alltime.innerHTML=timeFormat(document._video.duration);
+			document._video.addEventListener("timeupdate",getCurrentTime);
+			document._video.addEventListener("volumechange",volChanged);
+			clearInterval(i);
+		}
+	}, 200);
 }
 
 document.addEventListener("DOMContentLoaded", init, false);
@@ -58,17 +73,23 @@ function changeSpeed(bo){
 	}
 }
 
-//音量放大或缩小0.1
-function changeVolume(bo){
-	if(bo){
-		document._video.volume+=0.1;
-	}else{
-		document._video.volume-=0.1;
-	}
+//视频seek
+function videoSeekByClick(e){
+	alert("click");
+	alert(e.pageX);
 }
 
+//音量放大或缩小0.1
+// function changeVolume(bo){
+// 	if(bo){
+// 		document._video.volume+=0.1;
+// 	}else{
+// 		document._video.volume-=0.1;
+// 	}
+// }
+
 //音量设置到指定值
-function changeVolumeByNumber(vol){
+function changeVolumeByClick(e){
 	document._video.volume=vol;
 }
 
@@ -76,10 +97,10 @@ function changeVolumeByNumber(vol){
 function muted(){
 	if(document._video.muted){
 		document._video.muted=false;
-		document._mutedBtn.src = "images/soundoff.png";
+		document._mutedBtn.src = "images/soundon.png";
 	}else{
 		document._video.muted=true
-		document._mutedBtn.src = "images/soundon.png";
+		document._mutedBtn.src = "images/soundoff.png";
 	}
 }
 
@@ -92,6 +113,15 @@ function fullscreen(){
 	}
 }
 
+//音量改变
+function volChangee(){
+	if(document._video.muted){
+		document._mutedBtn.src = "images/soundoff.png";
+	}else{
+		document._mutedBtn.src = "images/soundon.png";
+	}
+}
+
 //全屏状态改变
 function screenChanged(){
 	if(document._video.webkitIsFullScreen){
@@ -99,6 +129,22 @@ function screenChanged(){
 	}else{
 		document._fullscreenBtn.src = "images/fullscreen.png";
 	}
+}
+
+//当前时间监听
+function getCurrentTime(){
+	document._currenttime.innerHTML=timeFormat(document._video.currentTime);
+}
+
+//转换时间格式
+function timeFormat(time){
+	var minutes = parseInt(time / 60, 10);
+	var seconds = parseInt(time % 60);
+	if(seconds<10){
+		seconds="0"+seconds;
+	}
+	var time_t=minutes+":"+seconds;
+	return time_t;
 }
 
 //显示log信息
